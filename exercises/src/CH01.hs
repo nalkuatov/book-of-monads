6ab6c8a6be86efce1c9c0f1ccb985a89db3208b5
Alkuatov Nurlan <alkuatovnurlan@gmail.com>
2020-08-02 22:23:31 +0600

    Add exercises for 1st chapter

---
+++ exercises/src/CH01.hs
@@ -0,0 +1,32 @@
+module CH01 where
+
+import Prelude hiding (pure, (++))
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+type State s a = s → (a, s)
+type WithCounter a = Int → (a, Int)
+
+next ∷ State s a → (a → State s b) → State s b
+f `next` g = (\i → let (x, i) = f i in g x i)
+
+pure ∷ a → WithCounter a
+pure x = (\i → (x, i))
+
+pure_ ∷ a → State s a
+pure_ x = (\i → (x, i))
+
+relabel ∷ Tree a → WithCounter (Tree (a, Int))
+relabel (Leaf x) = (\i → (Leaf (x, i), i + 1))
+relabel (Node l r) =
+     relabel l `next` (\l' → relabel r `next` (\r' → pure (Node l' r') ))
+
+relabel_ ∷ Tree a → State Int (Tree (a, Int))
+relabel_ (Leaf x) = (\i → (Leaf (x, i), i + 1))
+relabel_ (Node l r) =
+     relabel_ l `next` (\l' → relabel_ r `next` (\r' → pure_ (Node l' r') ))
+
+concat_ ∷ [a] → [a] → [a]
+[] `concat_` ys = ys
+(x : xs) `concat_` ys = x : (xs `concat_` ys)
+
