2ec2b45f4ff3e281149c95240cbdd3e9395a6c30
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-08-21 19:28:02 +0600

    Some experiments on compiler warnings

--- exercises/src/Experiments.hs
+++ exercises/src/Experiments.hs
@@ -1,8 +1,18 @@
 module Experiments where
 
+import Control.Monad
 
 failList ∷ [Int]
 failList = do
   (x : _) ← []
   return x
+
+-- | Ignore the result type contained in the monad
+void_ ∷ Functor f ⇒ f a → f ()
+void_ = fmap $ const ()
+
+ignore ∷ IO ()
+ignore =
+  void_ $
+  forM ["computers", "are", "the", "root", "of", "evil"] print
 
