269770fb55e16c6a8d934c546b32afd507a7b6cc
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-09-03 19:38:53 +0600

    Add chapter six

---
+++ exercises/src/CH06.hs
@@ -0,0 +1,12 @@
+module CH06 where
+
+-- | Exercise 6.1
+newtype State s a = State { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+  fmap f (State x) = State $ \s -> let (a, s') = x s in (f a, s')
+
+instance Applicative (State s) where
+  pure x = State $ \s -> (x, s)
+--  (State f) <*> State x = \s ->
+
