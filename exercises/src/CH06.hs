4b118ddc9a2695ece6ac0465fd7c27738f07e175
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-09-08 17:44:26 +0600

    Change chapter 6

--- exercises/src/CH06.hs
+++ exercises/src/CH06.hs
@@ -9,9 +9,24 @@ instance Functor (State s) where
 instance Applicative (State s) where
   pure x = State $ \s -> (x, s)
   State f <*> State x = State $ \s ->
-    let
-     (f', s')  = f s
-     (a,  s'') = x s'
-    in
-      (f' a, s'')
+    let (f', s')  = f s
+        (a,  s'') = x s'
+    in  (f' a, s'')
+
+instance Monad (State s) where
+  State x >>= f = State $ \s ->
+    let (x', s') = x s
+    in runState (f x') s'
+
+-- | Exercise 6.2
+get :: State s s
+get = State $ \s -> (s, s)
+
+put :: s -> State s ()
+put s = State $ const ((), s)
+
+modify :: (s -> s) -> State s ()
+modify f = do
+  s <- get
+  put $ f s
 
