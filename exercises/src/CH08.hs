5aff5afb512a304c6307c4e8ac0bfe49506d2a11
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-12-11 19:08:11 +0600

    Solve exercise 8.1

--- exercises/src/CH08.hs
+++ exercises/src/CH08.hs
@@ -1,4 +1,27 @@
 module CH08 where
 
+import           Control.Concurrent.STM.TVar
+import           Control.Monad               (guard)
+import           Control.Monad.STM
+
 -- Exercise 8.1
+_addName
+  :: TVar Integer
+  -> TVar [(Integer, String)]
+  -> String
+  -> STM ()
+_addName counter names name = do
+  i      <- readTVar counter
+  values <- readTVar names
+  case lookup i values of
+    Nothing -> do
+      modifyTVar names ((i, name) :)
+      writeTVar counter (i + 1)
+    Just _ -> pure ()
+
+addName :: String -> STM ()
+addName name = do
+  counter <- newTVar 0
+  names   <- newTVar []
+  _addName counter names name
 
