e33fc4c2c6caaf00849c0f2a9b501397d3cebdea
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-12-21 16:14:56 +0600

    Update chapter 10

--- exercises/src/CH10.hs
+++ exercises/src/CH10.hs
@@ -1,1 +1,21 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
+
 module CH10 where
+
+import           Control.Applicative (Alternative (..), liftA2)
+
+newtype (f :.: g) a = Compose (f (g a))
+
+instance (Functor f, Functor g) => Functor (f :.: g) where
+  fmap f (Compose x) = Compose $ fmap (fmap f) x
+
+instance (Applicative f, Applicative g) => Applicative (f :.: g) where
+  pure x = Compose $ pure $ pure x
+  Compose f <*> Compose x =
+    Compose $ liftA2 (<*>) f x
+
+instance (Alternative f, Applicative g) => Alternative (f :.: g) where
+  empty = Compose empty
+  Compose f <|> Compose g = Compose (f <|> g)
+
