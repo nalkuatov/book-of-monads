1cb77e89ae2d73f49e977684758fe6c9d673edce
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-01-02 12:43:16 +0600

    Solve 11.1

--- exercises/src/CH11.hs
+++ exercises/src/CH11.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module CH11 where
 
 import           Control.Monad.State
@@ -21,5 +23,20 @@ type Assignment = [(Name, Integer)]
 
 -- Exercise 11.1
 eval :: Expr -> MaybeT (State Assignment) Integer
-eval = undefined
+eval =
+  \case
+    Literal n -> pure n
+    Var  name -> MaybeT $ do
+      assignment <- get
+      pure $ lookup name assignment
+    Operation op first second -> do
+      x <- eval first
+      y <- eval second
+      case op of
+        Add      -> pure $ x + y
+        Subtract -> pure $ x - y
+        Multiply -> pure $ x * y
+        Divide   ->
+          if y == 0 then MaybeT $ pure Nothing
+          else pure $ x `div` y
 
