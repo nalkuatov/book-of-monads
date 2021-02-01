091c95daa7832e774d3d8ea0c003f1b2810ba186
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-02-01 19:30:58 +0600

    Remove unicode syntax from Experiments.hs

--- exercises/src/Experiments.hs
+++ exercises/src/Experiments.hs
@@ -2,16 +2,16 @@ module Experiments where
 
 import Control.Monad
 
-failList ∷ [Int]
+failList :: [Int]
 failList = do
-  (x : _) ← []
+  (x : _) <- []
   return x
 
 -- | Ignore the result type contained in the monad
-void_ ∷ Functor f ⇒ f a → f ()
+void_ :: Functor f => f a -> f ()
 void_ = fmap $ const ()
 
-ignore ∷ IO ()
+ignore :: IO ()
 ignore =
   void_ $
   forM ["computers", "are", "the", "root", "of", "evil"] print
