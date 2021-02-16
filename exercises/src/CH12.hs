783c20b402733a9f010d4746dd3050126d4a9280
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-02-16 19:46:07 +0600

    Continue solving 12.1

--- exercises/src/CH12.hs
+++ exercises/src/CH12.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes           #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module CH12 where
 
-import Control.Monad.Identity
+import           Control.Monad.Identity
+import           Control.Monad.Reader
 
---
 -- | Exercise 12.1
 class Monad m => MonadReader' r m | m -> r where
   ask'   :: m r
@@ -12,9 +13,15 @@ class Monad m => MonadReader' r m | m -> r where
   reader f = f <$> ask'
 
 class (Monoid w, Monad m) => MonadWriter' w m | m -> w where
-  tell :: w -> m ()
+  tell' :: w -> m ()
 
 class Monad m => MonadError' e m | m -> e where
   throwError' :: e -> m a
   catchError' :: m a -> (e -> m a) -> m a
+
+instance Monad m => MonadReader' r (ReaderT r m) where
+  ask' = ReaderT pure
+
+instance (MonadWriter' w m) => MonadWriter' w (ReaderT r m) where
+  tell' = ReaderT . const . tell'
 
