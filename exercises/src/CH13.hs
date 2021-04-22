e001d43c6d95b47f8ef9b00853b3e6cce183ba7a
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-04-22 18:26:00 +0600

    Solve 13.3

--- exercises/src/CH13.hs
+++ exercises/src/CH13.hs
@@ -1,5 +1,7 @@
 module CH13 where
 
+import           Prelude hiding (take)
+
 -- | Exercise 13.1: Complete the @Position@ data type below.
 -- It represents a position in the 3x3 board used to play tic-tac-toe.
 data Position =
@@ -21,4 +23,19 @@ data Result
 -- | Exercise 13.2
 -- An fictional game shown in a cinematic film
 -- about John Nash ("Beautiful Mind").
+
+-- | Exercise 13.3
+class TicTacToe m where
+  info :: Position -> m (Maybe Player)
+  take :: Position -> m Result
+
+takeIfNotTaken
+  :: (Monad m, TicTacToe m)
+  => Position
+  -> m (Maybe Result)
+takeIfNotTaken pos = do
+  p <- info pos
+  case p of
+    Just _  -> return Nothing
+    Nothing -> Just <$> take pos
 
