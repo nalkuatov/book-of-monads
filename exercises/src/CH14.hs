6449732094304bea3dd3e5dfaf13b00c9d1bd7f5
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-07-01 19:51:39 +0600

    Add more on coproduct of pattern functors

--- exercises/src/CH14.hs
+++ exercises/src/CH14.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators    #-}
 
 module CH14 where
+
+import           CH13 (FSF (..), Free (..))
 
 -- | Exercise 14.1
 data (f :+: g) a = InL (f a) | InR (g a)
@@ -8,4 +11,13 @@ data (f :+: g) a = InL (f a) | InR (g a)
 instance (Functor f, Functor g) => Functor (f :+: g) where
   fmap f (InL f') = InL $ fmap f f'
   fmap f (InR g)  = InR $ fmap f g
+
+data RandomGenF r = Next Int Int (Int -> r)
+
+class (f :<: g) where
+  inject :: f a -> g a
+
+randomWrite :: (Functor f, FSF :<: f, RandomGenF :<: f)
+  => Int -> Int -> Free f a
+randomWrite = undefined
 
